f95f57fb9d56b7a8582adf7747dfd77462574edd
ArgenisPalacios <Dr.Lfnt@gmail.com>
2020-08-06 16:48:26 -0500

    twitchelpMobile
    mobile assets

---
+++ twitchelpMobile/global.js
@@ -0,0 +1,17 @@
+/*$( document ).ready(function() {
+    //console.log( "ready!" );
+
+   
+
+});*/
+//var swiper = new Swiper('.swiper-container');
+alert("HOLAA");
+function toggleSearch (){
+    alert("toggle");
+    var box = document.getElementById ("searchBoxDiv");
+    if (box.style.display == "none"){
+        box.style.display = "inline-block"
+    }else{
+        box.style.display = "none"
+    }
+}
